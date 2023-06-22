252582815a33f08f3ca2771f2d1b90379e356c1b
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2023-06-22 17:26:39 -0500

    Added the Medida class

---
+++ src/main/java/com/oasisdrinks/app/model/Medida.java
@@ -0,0 +1,37 @@
+package com.oasisdrinks.app.model;
+
+public class Medida {
+    private int id;
+    private String nombre;
+    private String abrev;
+
+    public Medida() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getAbrev() {
+        return abrev;
+    }
+
+    public void setAbrev(String abrev) {
+        this.abrev = abrev;
+    }
+    
+    
+}
