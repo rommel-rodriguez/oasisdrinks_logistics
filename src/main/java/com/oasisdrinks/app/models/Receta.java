bfb7c29fa9c9190d09236e3f440867eaee324474
Rommel Rodriguez Perez <rommelrodperez@gmail.com>
2023-06-27 18:55:55 -0500

    Fixed RecetaDetalle

--- src/main/java/com/oasisdrinks/app/models/Receta.java
+++ src/main/java/com/oasisdrinks/app/models/Receta.java
@@ -1,36 +1,42 @@
 
 package com.oasisdrinks.app.models;
 
-public class Receta {
-    private String codReceta;
-    private int cantidad;
-    //private RecetaDetalle detalles ;
+import java.util.List;
 
-    /*public Receta(String codReceta, int cantidad, RecetaDetalle detalles) {
+public class Receta {
+    private int codReceta;
+    private int cantidad;
+    private List<RecetaDetalle> detalles ;
+
+    public Receta(int codReceta, int cantidad, List<RecetaDetalle> detalles) {
     this.codReceta = codReceta;    
     this.cantidad = cantidad;
     this.detalles = detalles;
-    }*/
-    public Receta(String codReceta, int cantidad) {
+    }
+
+    public Receta(int codReceta, int cantidad) {
     this.codReceta = codReceta;    
     this.cantidad = cantidad;
     }
     
-    public String getCodReceta() {return codReceta;}
-    public void setCodReceta(String codReceta) {this.codReceta = codReceta;}
+    public int getCodReceta() {return codReceta;}
+
+    public void setCodReceta(int codReceta) {this.codReceta = codReceta;}
 
     public int getCantidad() {return cantidad;}
     public void setCantidad(int cantidad) {this.cantidad = cantidad;    }
 
-    /*public RecetaDetalle getdetalles(){
+    public List<RecetaDetalle> getdetalles(){
         return detalles;
     }
-        public void setRecetaDetalle(RecetaDetalle detalles){
-        this.detalles= detalles;
-    }    
-    public void agregarDetalle (RecetaDetalle x){
-        detalles.add(x);
-    }*/
+
+    //    public void setRecetaDetalle(List<RecetaDetalle> detalles){
+    //    this.detalles= detalles;
+    //}    
+
+    public void agregarDetalle (RecetaDetalle detalle){
+        detalles.add(detalle);
+    }
 
     @Override
     public String toString() {
